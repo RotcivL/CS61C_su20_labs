a25bbdc522bc6322998a7c86e2158928d829f8a0
RotcivL <victor.lee.98.vic@gmail.com>
2023-09-05 22:11:52 +1000

    complete lab03

--- lab03/factorial.s
+++ lab03/factorial.s
@@ -20,5 +20,32 @@ main:
     addi a0, x0, 10
     ecall # Exit
 
+# c code
+#if (n == 1) {
+#        return 1
+#    }
+#   return factorial(n-1) * n
 factorial:
     # YOUR CODE HERE
+    # check for base case n == 1
+    li t0, 1
+    beq a0, t0, end  # jump to end if n == 1, a0=1 already
+
+    # prologue save retaddr, current n
+    addi sp, sp, -8
+    sw ra, 4(sp)
+    sw a0, 0(sp)
+
+    # recursive call
+    addi a0, a0, -1
+    jal ra, factorial
+
+    # epilogue
+    lw t0, 0(sp)  # get n
+    lw ra, 4(sp)
+    addi sp, sp, 8
+
+    # factorial(n-1)*n
+    mul a0, a0, t0
+end:
+    ret
